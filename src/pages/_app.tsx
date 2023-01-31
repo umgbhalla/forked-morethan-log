1bc081359b16e7d73487b005b0023013ad8e95a1
Umang Bhalla <umg.bhalla88@gmail.com>
2023-01-31 17:13:49 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -25,7 +25,18 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
 
   return (
     <>
-      <Scripts />
+      <Script
+        id="clar"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", 'fmuqcdi0aq');`,
+        }}
+      />
       {getLayout(<Component {...pageProps} />)}
     </>
   )
